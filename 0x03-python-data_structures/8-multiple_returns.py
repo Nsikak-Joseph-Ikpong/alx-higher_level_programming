e226d2fe4054d40736781e47199366584cf88de7
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-12-04 13:57:28 -0800

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    string_length = len(sentence)
-
-    if string_length == 0:
+    length = len(sentence)
+    if length == 0:
         first_char = None
-        _tuple = (string_length, first_char)
-        return  _tuple
+        _tuple = (length, first_char)
+        return _tuple
     else:
         first_char = sentence[0]
-        _tuple = (string_length, first_char)
+        _tuple = (length, first_char)
         return _tuple
