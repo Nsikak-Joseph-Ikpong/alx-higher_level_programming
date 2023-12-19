29fbcdb3e34827c6ac70852d7ac6aeae481481d0
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-12-19 11:47:33 -0800

    Update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """A class that defines a square"""
+
+
 class Square:
+      """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
         """A new instance of a square"""
         self.__size = size
